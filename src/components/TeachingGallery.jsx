8d282742d29c71ccefcfbc1f1e8c1f3d8bc6db79
Nambhi Amarnath <116098792+Amarnath2541@users.noreply.github.com>
2026-07-25 23:34:16 +0530

    Remove duplicate images and add new ones

--- src/components/TeachingGallery.jsx
+++ src/components/TeachingGallery.jsx
@@ -36,10 +36,10 @@ const TeachingGallery = () => {
     { src: '/images/teach4.jpeg' },
     { src: '/images/teach5.jpg' },
     { src: '/images/teach6.jpg' },
-    { src: '/images/teach6.jpg' },
-    { src: '/images/teach6.jpg' },
-    { src: '/images/teach6.jpg' },
-    { src: '/images/teach6.jpg' },
+    { src: '/images/teach7.jpg' },
+    { src: '/images/teach8.jpg' },
+    { src: '/images/teach9.jpg' },
+    { src: '/images/teach10.jpg' },
   ];
 
   const openModal = (image) => {
